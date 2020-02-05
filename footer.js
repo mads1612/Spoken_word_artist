733ca5b8d4487b1a0146282150991c63a68c0166
Mads Iversen <madsiversen@hotmail.com>
2020-02-05 12:43:48 +0100

    formindsket indhold

--- footer.js
+++ footer.js
@@ -21,13 +21,9 @@
  }
 
  function showFooter() {
-     document.querySelector(".footer_overskrift").textContent = sideFooter.footer_overskrift;
-     document.querySelector(".tlf_tekst").textContent = sideFooter.footer_tekst_tlf;
-     document.querySelector(".map_tekst").textContent = sideFooter.footer_tekst_adress;
-     document.querySelector(".email_tekst").textContent = sideFooter.footer_tekst_email;
-     document.querySelector(".email").src = sideFooter.footer_mail_ikon.guid;
-     document.querySelector(".tlf").src = sideFooter.footer_tlf_ikon.guid;
-     document.querySelector(".map").src = sideFooter.footer_map_ikon.guid;
+     document.querySelector(".navn").textContent = sideFooter.footer_navn;
+     document.querySelector(".titel").textContent = sideFooter.footer_titel;
+
      document.querySelector(".face").src = sideFooter.footer_face_ikon.guid;
      document.querySelector(".insta").src = sideFooter.footer_insta_ikon.guid;
 
